d673c64f8433ab41bf4015e2494a68af99bd149b
Alexander Kochergin <ejiqpep@gmail.com>
2014-11-14 18:55:14 +0200

    Fix adapter for hm

--- js/adapter.js
+++ js/adapter.js
@@ -3,3 +3,18 @@ App.LSAdapter = DS.LSAdapter.extend({
 });
 
 App.ApplicationAdapter = App.LSAdapter;
+
+
+
+DS.JSONSerializer.reopen({
+    serializeHasMany : function(record, json, relationship) {
+        var key = relationship.key;
+
+        var relationshipType = record.constructor.determineRelationshipType(
+                record.constructor, relationship);
+
+        if (relationshipType === 'manyToNone' || relationshipType === 'manyToMany' || relationshipType === 'manyToOne') {
+            json[key] = Ember.get(record, key).mapBy('id');
+        }
+    }
+});
