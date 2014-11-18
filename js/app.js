888911c2ccc4a8200a377f5d641c49ddd1d0d734
Alexander Kochergin <ejiqpep@gmail.com>
2014-11-18 15:28:03 +0200

    Added firebase

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 App = Ember.Application.create();
-App.ApplicationAdapter = DS.FixtureAdapter;
+// App.ApplicationAdapter = DS.FixtureAdapter;
 
+App.ApplicationAdapter = DS.FirebaseAdapter.extend({
+  firebase: new Firebase('https://flickering-heat-9887.firebaseio.com/')
+});
